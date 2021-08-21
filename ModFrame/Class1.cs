d782f95de7cb8962c34538ddb582afe144efd6f7
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2021-08-20 20:51:12 -0500

    work

--- ModFrame/Class1.cs
+++ ModFrame/Class1.cs
@@ -1,21 +1,90 @@
-﻿using System.Reflection;
+﻿using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection;
 using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
+using UnityEngine;
 
-namespace ModFrame
+namespace HudReplacer
 {
     [BepInPlugin(ModGUID, ModName, ModVersion)]
-    public class NewMod : BaseUnityPlugin
+    public class HudReplacer : BaseUnityPlugin
     {
-        private const string ModName = "New Mod";
-        private const string ModVersion = "1.0";
-        private const string ModGUID = "some.new.guid";
-    
+        private const string ModName = "HudReplacer";
+        private const string ModVersion = "2.0";
+        private const string ModGUID = "com.zarboz.hudreplacer";
+
+        private static GameObject NewHud;
+        private static AssetBundle hudbundle;
+        internal static GameObject HudSwap;
+        private static ConfigEntry<Vector3> HudLocation;
+        private Vector3 barPostion;
+
         public void Awake()
         {
             Assembly assembly = Assembly.GetExecutingAssembly();
             Harmony harmony = new(ModGUID);
             harmony.PatchAll(assembly);
+            LoadAssets();
+            HudLocation = Config.Bind("General", "SkillBar Location", barPostion,
+                new ConfigDescription("Location of SkillBar"));
+         }
+
+        public void Update()
+        {
+            if (Player.m_localPlayer == null)
+                return;
+            HealthBar.CurHealthValue = Player.m_localPlayer.GetHealth();
+            HealthBar.MaxHealthValue = Player.m_localPlayer.GetMaxHealth(); 
+            GuardianHUD.GetGuardianPower();
+            if (Draggable.isMouseDown)
+            {
+                barPostion = HudSwap.transform.localPosition;
+                HudLocation.Value = barPostion;
+                Config.Save();
+            }
+
+        }
+        
+
+        public void LoadAssets()
+        {
+            hudbundle = GetAssetBundleFromResources("stockreplacer");
+            NewHud = hudbundle.LoadAsset<GameObject>("StockReplacer");
+        }
+        private static AssetBundle GetAssetBundleFromResources(string filename)
+        {
+            var execAssembly = Assembly.GetExecutingAssembly();
+            var resourceName = execAssembly.GetManifestResourceNames()
+                .Single(str => str.EndsWith(filename));
+
+            using (var stream = execAssembly.GetManifestResourceStream(resourceName))
+            {
+                return AssetBundle.LoadFromStream(stream);
+            }
+        }
+        
+        [HarmonyPatch(typeof(Hud), nameof(Hud.Awake))]
+        public static class HUDPatch
+        {
+
+
+            public static void Postfix(Hud __instance)
+            {
+                __instance.m_gpIcon.gameObject.SetActive(false);
+                __instance.m_gpCooldown.gameObject.SetActive(false);
+                __instance.m_gpName.gameObject.SetActive(false);
+                __instance.m_foodBarRoot.gameObject.SetActive(false);
+                __instance.m_healthPanel.gameObject.SetActive(false);
+                HudSwap.transform.SetSiblingIndex(__instance.m_gpRoot.transform.GetSiblingIndex());
+              }
+
+            public static void Prefix(Hud __instance)
+            {
+                HudSwap = Instantiate(NewHud, __instance.gameObject.transform, false); 
+                HudSwap.transform.localPosition = HudLocation.Value;
+            }
         }
     }
 }
---
+++ ModFrame/GuardianHud.cs
@@ -0,0 +1,40 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+namespace HudReplacer
+{
+    public class GuardianHUD
+    {
+        private static StatusEffect _guardianPower;
+        private static float _cooldown;
+        [CanBeNull] internal static Localize _localize;
+        internal string GP_text;
+        
+
+        internal static void GetGuardianPower()
+        {
+            Player.m_localPlayer.GetGuardianPowerHUD(out _guardianPower, out _cooldown);
+            if (_guardianPower == null)
+            {
+                GDPower.Gpowerindicator = false;
+            }
+            else if (_guardianPower != null)
+            {
+                GDPower.Gpowerindicator = true;
+                GDPower.GuardianSprite = _guardianPower.m_icon;
+                GDPower.CooldownTxt = TimeFomat(Mathf.CeilToInt(_cooldown));
+                GDPower.GDName = Localization.instance.Localize(_guardianPower.m_name);
+                GDPower.GDKey = Localization.instance.Localize("$KEY_GPower");
+                GDPower.rawcooldown = _cooldown;
+            }
+        }
+        
+        public static string TimeFomat(int secs)
+        {
+            int mins = (secs % 3600) / 60;
+            secs = secs % 60;
+            return $"{mins:00}:{secs:00}";
+        }
+    }
+}
