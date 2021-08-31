05f292fba78833e2e2152c27b7595f7b9c358b0c
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2021-08-30 22:00:21 -0500

    add neat config options

--- ModFrame/Class1.cs
+++ ModFrame/Class1.cs
@@ -17,9 +17,15 @@ namespace HudReplacer
 
         private static GameObject NewHud;
         private static AssetBundle hudbundle;
-        internal static GameObject HudSwap;
+        private static GameObject HudSwap;
         private static ConfigEntry<Vector3> HudLocation;
+        private static ConfigEntry<Color> HealthBG;
+        private static ConfigEntry<Color> HealthHotline;
+        private static ConfigEntry<Color> UnfilledBG;
         private Vector3 barPostion;
+        private static readonly int MainColor = Shader.PropertyToID("_MainColor");
+        private static readonly int HotlineColor = Shader.PropertyToID("_HotLineColor");
+        private static readonly int UnfilledColor = Shader.PropertyToID("_AlphaColor");
 
         public void Awake()
         {
@@ -29,7 +35,13 @@ namespace HudReplacer
             LoadAssets();
             HudLocation = Config.Bind("General", "SkillBar Location", barPostion,
                 new ConfigDescription("Location of SkillBar"));
-         }
+            HealthBG = Config.Bind("General", "SkillBar Health Background", new Color(0.8679f, 0f, 0.0281f, 1f),
+                "The Color of the Healthbar filler");
+            HealthHotline = Config.Bind("General", "SkillBar Health Hotline Color", new Color(0f, 0.7053f, 1f, 1f),
+                "The Color of the Healthbar Hotline or the burn line at the top of the health when it is low or filling");
+            UnfilledBG = Config.Bind("General", "SkillBar Health Unfilled Color", new Color(0f, 0f, 0f, 0f),
+                "The Color of the unfilled portion of the Healthbar background");
+        }
 
         public void Update()
         {
@@ -78,7 +90,9 @@ namespace HudReplacer
                 __instance.m_foodBarRoot.gameObject.SetActive(false);
                 __instance.m_healthPanel.gameObject.SetActive(false);
                 HudSwap.transform.SetSiblingIndex(__instance.m_gpRoot.transform.GetSiblingIndex());
-                //Maybe we can add a color edit toggle here cuz its neat
+                HealthBar.coolmatstatic.SetColor(MainColor, HealthBG.Value);
+                HealthBar.coolmatstatic.SetColor(HotlineColor, HealthHotline.Value);
+                HealthBar.coolmatstatic.SetColor(UnfilledColor, UnfilledBG.Value);
               }
 
             public static void Prefix(Hud __instance)
